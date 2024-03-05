21cd16dd4ebc522d491e466ed0f65d4958d5ecf1
jagger <1336098842@qq.com>
2024-03-05 19:38:53 +0800

    first commit

---
+++ lib/jagger_judicatory_test.rb
@@ -0,0 +1,6 @@
+require "jagger_judicatory_test/version"
+
+module JaggerJudicatoryTest
+  class Error < StandardError; end
+  # Your code goes here...
+end
---
+++ lib/jagger_judicatory_test/version.rb
@@ -0,0 +1,3 @@
+module JaggerJudicatoryTest
+  VERSION = "0.1.1"
+end
